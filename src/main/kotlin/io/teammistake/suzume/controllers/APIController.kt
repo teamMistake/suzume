995d0313a1abecc96cb1e673781d6b083d18de0b
syeyoung <cyoung06@naver.com>
2023-07-04 19:14:23 +0900

    - add @RequestBody

--- src/main/kotlin/io/teammistake/suzume/controllers/APIController.kt
+++ src/main/kotlin/io/teammistake/suzume/controllers/APIController.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType.*
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -14,15 +15,15 @@ import reactor.core.publisher.Mono
 @RestController
 class APIController {
     @Autowired lateinit var messageQueueService: MessageQueueService;
-    @PostMapping("/stream", produces = [APPLICATION_NDJSON_VALUE, TEXT_EVENT_STREAM_VALUE])
-    suspend fun generateResponse(request: APIInferenceRequest): Flux<Any> {
+    @PostMapping("/stream", produces = [APPLICATION_NDJSON_VALUE, TEXT_EVENT_STREAM_VALUE], consumes = [APPLICATION_JSON_VALUE])
+    suspend fun generateResponse(@RequestBody request: APIInferenceRequest): Flux<Any> {
         if (!request.stream) throw IllegalArgumentException("Invalid Accept Header, should be either $APPLICATION_NDJSON_VALUE or $TEXT_EVENT_STREAM_VALUE")
         val pair = messageQueueService.request(request);
         return Flux.concat(Mono.just(pair.second), pair.first);
     }
 
-    @PostMapping("/stream", produces = [ APPLICATION_JSON_VALUE])
-    suspend fun generateResponseJson(request: APIInferenceRequest): APIResponse {
+    @PostMapping("/generate", produces = [ APPLICATION_JSON_VALUE], consumes = [APPLICATION_JSON_VALUE])
+    suspend fun generateResponseJson(@RequestBody request: APIInferenceRequest): APIResponse {
         if (request.stream) throw IllegalArgumentException("Invalid Accept Header, should be $APPLICATION_JSON_VALUE")
 
         val pair = messageQueueService.request(request);
