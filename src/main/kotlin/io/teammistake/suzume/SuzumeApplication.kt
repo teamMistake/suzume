9c825dad4f2c24f7cf7915e004404c20453c51a6
syeyoung <cyoung06@naver.com>
2023-07-07 12:51:32 +0900

    - disable mongodb autoconfig

--- src/main/kotlin/io/teammistake/suzume/SuzumeApplication.kt
+++ src/main/kotlin/io/teammistake/suzume/SuzumeApplication.kt
@@ -1,6 +1,8 @@
 package io.teammistake.suzume
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
@@ -8,6 +10,7 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @SpringBootApplication
 @EnableReactiveMongoRepositories
 @EnableMongoRepositories
+@EnableAutoConfiguration(exclude=[MongoAutoConfiguration::class])
 class SuzumeApplication
 
 fun main(args: Array<String>) {
