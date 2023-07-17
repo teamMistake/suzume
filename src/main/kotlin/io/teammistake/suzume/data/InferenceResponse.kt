3caab2c8b8447777559496ce379fd230f2f1944e
syeyoung <cyoung06@naver.com>
2023-07-17 14:25:08 +0900

    - Include types

--- src/main/kotlin/io/teammistake/suzume/data/InferenceResponse.kt
+++ src/main/kotlin/io/teammistake/suzume/data/InferenceResponse.kt
@@ -1,6 +1,8 @@
 package io.teammistake.suzume.data
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
@@ -12,7 +14,7 @@ data class InferenceResponse(
     val respFull: String?,
     val eos: Boolean,
     val error: String? = null
-)
+): SuzumeResponse()
 
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class APIResponse(
@@ -25,4 +27,10 @@ data class APIResponse(
 data class APIResponseHeader(
     val reqId: String,
     val model: String
+): SuzumeResponse()
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes(
+    JsonSubTypes.Type(name="header", value=APIResponseHeader::class),
+    JsonSubTypes.Type(name="response", value=InferenceResponse::class)
 )
+open class SuzumeResponse;
